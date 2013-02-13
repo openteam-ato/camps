dcb3afe26f73e96173a8cb026b814556a4946862
tab <tukhfatullin@gmail.com>
2013-02-13 11:26:36 +0700

    Right smtp settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
+domain   = Settings['mail']['domain']
+smtp     = Settings['mail']['smtp']
+port     = Settings['mail']['port']
+
 ActionMailer::Base.smtp_settings = {
-  :address              => Settings['mail']['address'],
-  :port                 => Settings['mail']['port'],
-  :domain               => Settings['mail']['domain'],
-  :user_name            => Settings['mail']['login'],
-  :password             => Settings['mail']['password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :address              => smtp,
+  :port                 => port,
+  :domain               => domain
 }
