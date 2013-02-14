5428bdb41b27d314fd07738f672ea2f2f3a8039b
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-02-14 12:03:47 +0700

    Render navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
       render :partial => 'regions/error', :locals => { :region => region }
     end
   end
+
+  def render_navigation(hash, exclude_first = false)
+    return '' if hash.nil? || hash.empty?
+
+    content_tag :ul do
+      hash.map do |key, value|
+        content_tag :li, :class => value['selected'] ? 'selected' : nil do
+          link_to(value['title'], value['path'])+render_navigation(value['children'])
+        end
+      end.join.html_safe
+    end
+  end
 end
